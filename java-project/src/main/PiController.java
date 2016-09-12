a89ffd1bdb64f1de12e760a4d526928e795f1eb9
Dylan Chong <dylan.chong@icloud.com>
2016-09-12 20:47:34 +1200

    Tidied up, change Runtime to ProcessBuilder

--- java-project/src/main/PiController.java
+++ java-project/src/main/PiController.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
@@ -8,11 +9,13 @@ import java.util.Scanner;
  */
 class PiController {
     private static final String PI_IP = "10.140.108.72";
+
     private static PiController instance;
     private Process sshProcess;
     private boolean isRunning = false;
 
-    private PiController() { }
+    private PiController() {
+    }
 
     static PiController getInstance() {
         if (instance == null) instance = new PiController();
@@ -25,27 +28,68 @@ class PiController {
         if (isRunning) throw new AlreadyRunningException();
         assert PI_IP.length() > 8; // make sure IP exists
 
-        if (sshProcess == null) {
-            sshProcess = Runtime.getRuntime().exec("TODO");
-            // TODO NEXT run SSH command
+
+        // TODO NEXT test processbuilder - see if can run more commadns
+        {
+            String cmd = "ls"; // TODO run SSH command
+            // sshProcess = Runtime.getRuntime().exec(cmd);
+            ProcessBuilder processBuilder = new ProcessBuilder("ls");
+            sshProcess = processBuilder.start();
         }
 
         Scanner terminalScanner = new Scanner(sshProcess.getInputStream());
+        PrintStream terminalStream = new PrintStream(sshProcess.getOutputStream());
 
         (new Thread(() -> {
             isRunning = true;
 
-            while (terminalScanner.hasNext()) {
-                System.out.println("\tTERMINAL OUT: " +
-                        terminalScanner.nextLine());
-            }
+            sshToPi(terminalScanner, terminalStream);
 
-            // TODO AFTER watch output and respond to it TO SSH IN
-
-            // Done
-            doneCallback.run();
+            terminalScanner.close();
+            terminalStream.close();
             isRunning = false;
+            doneCallback.run();
         })).start();
+    }
+
+    /**
+     * Should only be called by sendDataToPi()
+     *
+     * @param terminalScanner For reading terminal
+     * @param terminalStream  For writing commands to terminal
+     */
+    private void sshToPi(Scanner terminalScanner, PrintStream terminalStream) {
+        // while (terminalScanner.hasNext()) {
+        for (int a = 0; a < 2; a++) {
+            logTerminalOutput(terminalScanner.nextLine());
+        }
+
+        runTerminalCommand("ls -a", terminalStream);
+        terminalStream.flush();
+        terminalStream.close();
+
+        while (true) {
+            if (terminalScanner.hasNext())
+                logTerminalOutput(terminalScanner.nextLine());
+        }
+        // TODO watch output and respond to it TO SSH IN
+    }
+
+    /**
+     * @param line A line that was read from the terminal
+     */
+    private void logTerminalOutput(String line) {
+        System.out.println("\tTERMINAL OUT: " + line);
+    }
+
+    /**
+     * Does not flush!
+     *
+     * @param command A command to write to the terminal
+     */
+    private void runTerminalCommand(String command, PrintStream ps) {
+        System.out.println("\tTERMINAL INP: " + command);
+        ps.println(command);
     }
 
     private class AlreadyRunningException extends Exception {
